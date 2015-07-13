e7a2e8c59eebbc0cb11d5413d329e6690cf67909
Daniel Gutiérrez <gutizar@gmail.com>
2015-07-13 20:08:40 +0200

    Working on the order main page

--- hobiles/public/javascripts/hobiles.controllers.js
+++ hobiles/public/javascripts/hobiles.controllers.js
@@ -8,16 +8,22 @@
 angular.module('hobiles.controllers', []).
 	controller('HomeCtrl', ['$scope', 'orders', function ($scope, orders) {
 		$scope.message = 'Hello, World!';
+		
 		$scope.orders = orders.orders;
 	}]).
 	controller('OrderCtrl', ['$scope', 'orders', 'order', function ($scope, orders, order) {
 		$scope.order = order;
 
 		$scope.addComment = function () {
-			if (!$scope.comment || !$scope.comment.body || !$scope.comment.createdBy) {
+			if (!$scope.body || !$scope.createdBy) {
+				// Return some validation error
 				return;
 			}
-			orders.addComment($scope.order, $scope.comment);
+			orders.addComment($scope.order, {
+				body: $scope.body, createdBy: $scope.createdBy
+			});
+			$scope.body = '';
+			$scope.createdBy = '';
 		};
 
 		$scope.toggle = function () {
