e29e6974ea5cb5a591bca8f83caa0c9cb458d5a6
Daniel Gutiérrez <gutizar@gmail.com>
2015-07-14 13:32:39 +0200

    Started with the user-relation backend actions.

--- hobiles/routes/users.js
+++ hobiles/routes/users.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/register', function (req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({
+      message: 'Please fill out all the fields'
+    });
+  }
+
+  var user;
+  user.username = req.body.username;
+  user.setPassword(req.body.password);
+  user.save(function (err) {
+    if (err) { return next(err); }
+    return res.json({
+      token: user.generateJWT()
+    });
+  });
+});
+
 module.exports = router;
